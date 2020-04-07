60a6d97ce24e1c29ef2191522a6ab606d3ac229d
Rooney <Rooney@192.168.86.36>
2020-04-06 23:24:58 -0400

    easy locked new added

---
+++ Preparation/src/Easy/LoggerRateLimiter.java
@@ -0,0 +1,28 @@
+package Easy;
+
+import java.util.*;
+
+public class LoggerRateLimiter {
+private HashMap<String, Integer> messagePrintTime = new HashMap<String, Integer>();
+    
+    
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(messagePrintTime.containsKey(message)) {
+            int time = messagePrintTime.get(message);
+            //System.out.println(message + " " + time + " " + timestamp);
+            if(timestamp - time >= 10) {
+                messagePrintTime.put(message, timestamp);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            messagePrintTime.put(message, timestamp);
+            return true;
+        }
+    }
+}
---
+++ Preparation/src/Easy/ValidWordAbbreviation.java
@@ -0,0 +1,40 @@
+package Easy;
+
+public class ValidWordAbbreviation {
+	public boolean validWordAbbreviation(String word, String abbr) {
+        if (word.isEmpty()) {
+            return abbr.isEmpty();
+        }
+ 
+        int pWord = 0;
+        int pAbbr = 0;
+ 
+        while (pWord < word.length() && pAbbr < abbr.length()) {
+            if (!Character.isDigit(abbr.charAt(pAbbr))) {
+                if (word.charAt(pWord) == abbr.charAt(pAbbr)) {
+                    pWord++;
+                    pAbbr++;
+                } else {
+                    return false;
+                }
+            } else {           
+                // edge case: leading zero
+                //
+                if (abbr.charAt(pAbbr) == '0') {
+                    return false;
+                }
+                 
+                int num = 0;
+                while (pAbbr < abbr.length() && Character.isDigit(abbr.charAt(pAbbr))) {
+                    int digit = Character.getNumericValue(abbr.charAt(pAbbr));
+                    num = num * 10 + digit;
+                    pAbbr++;
+                }
+ 
+                pWord += num;
+            }
+        }
+ 
+        return pWord == word.length() && pAbbr == abbr.length();
+    }
+}
