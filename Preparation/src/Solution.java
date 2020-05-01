b72c448c6771b3ba7332738eb803d843cb83ee32
Rooney <Rooney@192.168.86.63>
2020-05-01 01:32:12 -0400

    SQL Easy completed

--- Preparation/src/Solution.java
+++ Preparation/src/Solution.java
@@ -5,59 +5,40 @@ class Solution {
 	
 	public static void main(String[] args)
 	{
-		minCut("aab");
-}
-	
-	public static int minCut(String word){
-        int[][] matrix = new int[word.length()][word.length()];
-
-			for (int length = 1; length <= word.length(); length++)
-			{
-				for (int i = 0; i < word.length() - length; i++)
-				{
-					int j = i + length - 1;
-					if (isPalindrome(word.substring(i, length)))
-					{
-						matrix[i][j] = 0;
-					}
-					else
-					{
-						matrix[i][j] = GetMinimumBetween(matrix, i, j);
-					}
-				}
-			}
-
-			return matrix[0][word.length() - 1];
-    }
-    
-    private static int GetMinimumBetween(int[][] matrix, int i, int j)
-		{
-			int value = 1 + matrix[i][i] + matrix[i + 1][j];
-			int min = value;
-
-			for (int k = i; k <= j - 1; k++)
-			{
-				value = 1 + matrix[i][k] + matrix[k + 1][j];
-				if (min > value)
-					min = value;
-			}
-
-			return min;
-		}
-    
-    private static boolean isPalindrome(String str) {
-	int left = 0;
-	int right = str.length() - 1;
- 
-	while (left < right) {
-		if (str.charAt(left) != str.charAt(right)) {
-			return false;
-		}
- 
-		left++;
-		right--;
+		Solution s = new Solution();
+		int a = s.findPaths(2,2,2,0,0);
 	}
- 
-	return true;
-}
+	
+	public int findPaths(int m, int n, int N, int i, int j) {
+        if (N <= 0) return 0;
+        
+        final int MOD = 1000000007;
+        int[][] count = new int[m][n];
+        count[i][j] = 1;
+        int result = 0;
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int step = 0; step < N; step++) {
+            int[][] temp = new int[m][n];
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    for (int[] d : dirs) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                            result = (result + count[r][c]) % MOD;
+                        }
+                        else {
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                        }
+                    }
+                }
+            }
+            count = temp;
+        }
+        
+        return result;
+    }
+	
 }
